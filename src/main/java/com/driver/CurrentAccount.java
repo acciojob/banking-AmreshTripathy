e49df1dba26be494a4120f1f25c53a33521a80ff
AmreshTripathy <amreshtripathy184@gmail.com>
2023-05-31 22:30:37 +0530

    Bank Push-1

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,17 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount{
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrentAccount extends BankAccount {
     String tradeLicenseId; //consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, balance, 5000);
+        if (balance < 5000)
+            throw new Exception("Insufficient Balance");
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +19,16 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        Map<Character, Integer> map = new HashMap<>();
+        int maxFrequency = 0;
+        for (int i = 0; i < tradeLicenseId.length(); i++) {
+            char ch = tradeLicenseId.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            maxFrequency = Math.max(maxFrequency, map.get(ch));
+        }
 
+        if (maxFrequency > tradeLicenseId.length() / 2)
+            throw new Exception("Valid License can not be generated");
     }
 
 }
--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -1,28 +1,37 @@
 package com.driver;
 
-public class SavingsAccount extends BankAccount{
+public class SavingsAccount extends BankAccount {
     double rate;
     double maxWithdrawalLimit;
 
+
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
-
+        super(name, balance, 0);
+        this.maxWithdrawalLimit = maxWithdrawalLimit;
+        this.rate = rate;
     }
+
     public void withdraw(double amount) throws Exception {
         // Might throw the following errors:
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
-
+        if (amount > maxWithdrawalLimit)
+            throw new Exception("Maximum Withdraw Limit Exceed");
+        else if (amount > super.getBalance())
+            throw new Exception("Insufficient Balance");
+        else {
+            super.setBalance(super.getBalance() - amount);
+        }
     }
 
-    public double getSimpleInterest(int years){
+    public double getSimpleInterest(int years) {
         // Return the final amount considering that bank gives simple interest on current amount
-
+        return (super.getBalance() * rate * years) / 100;
     }
 
-    public double getCompoundInterest(int times, int years){
+    public double getCompoundInterest(int times, int years) {
         // Return the final amount considering that bank gives compound interest on current amount given times per year
-
+        return (super.getBalance() * (Math.pow((1 + rate / 100), times))) / years;
     }
-
 }
